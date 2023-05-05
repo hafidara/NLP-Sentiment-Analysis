10bda5b475466b82086406714d06ceac967c7379
Hafida Rahmane <59095444+hafidara@users.noreply.github.com>
2023-05-05 09:48:18 +0000

    added neutral

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ import pandas as pd
 df = pd.read_csv('sentiments.csv')
 df['blob'] = df['text'].apply(lambda x: TextBlob(x))
 df['polarity'] = df['blob'].apply(lambda x: x.sentiment.polarity)
-df['sentiment'] = df['polarity'].apply(lambda x: 'positive' if x > 0 else 'negative' if x < 0 else 'neutral')
+
+def get_sentiment(polarity):
+    if polarity > 0:
+        return 'positive'
+    elif polarity < 0:
+        return 'negative'
+    else:
+        return 'neutral'
+df['sentiment'] = df['polarity'].apply(get_sentiment)
 for index, row in df.iterrows():
     print(f"Row {index+1}: {row['sentiment'].capitalize()}")
+print(df)
 sentence = input("Enter a sentence: ")
 blob = TextBlob(sentence)
 polarity = blob.sentiment.polarity
 sentiment = 'positive' if polarity > 0 else 'negative' if polarity < 0 else 'neutral'
 print(f"The sentiment of '{sentence}' is {sentiment}.")
-# print(df)
+
